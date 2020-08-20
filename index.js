02dd261edccfa6e9e982d89e6dd6862f0471e02e
rinat-gk <rinatgk01@gmail.com>
2020-08-20 12:44:25 -0700

    Reconfigure

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const express = require('express');
-
-const reds = require('redis');
+const express = require("express");
+const redis = require("redis");
+const process = require("process");
 
 const app = express();
-
 const client = redis.createClient({
-    host: 'redis-server',
-    host: 6379
-
+  host: "redis-server",
+  port: 6379
 });
+client.set("visits", 0);
 
-client.set('visits', 0);
-
-
-app.get('/', (req, res) => {
-    client.get('visits', (err, visits) => {
-        res.send('Number of visit is' + visits);
-        client.set('visits', parseInt(visits) + 1);
-    
-    });
-
+app.get("/", (req, res) => {
+  //process.exit(0)
+  client.get("visits", (err, visits) => {
+    res.send("Number of visits " + visits);
+    client.set("visits", parseInt(visits) + 1);
+  });
 });
 
 app.listen(8081, () => {
-    console.log('Listening on port 8081');
+  console.log("listening on port 8081");
 });
